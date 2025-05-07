4f68f83161ee216d2043379d10ad9299d329ea5e
ingediego94 <inge.diego94@gmail.com>
2025-05-06 22:33:19 -0500

    Se adelanta el ejercicio de la S3

--- Tareas/Semana_3/EntrenamientoM1S3-DiegoVallejo copy.py
+++ Tareas/Semana_3/EntrenamientoM1S3-DiegoVallejo copy.py
@@ -12,9 +12,9 @@ products = []
 # Function to add products
 
 def add_product():
-    name = input("Ingresa el nombre producto: ")
-    price = float(input("Ingresa el precio del producto: "))
-    quantity = int(input("Ingresa la cantidad de productos: "))
+    name = input("Ingresa el nombre producto:  ").lower()
+    price = float(input("Ingresa el precio del producto:  $ "))
+    quantity = int(input("Ingresa la cantidad de productos:  "))
 
     product_dic = {
         'producto': name,
@@ -22,10 +22,12 @@ def add_product():
         'cantidad': quantity
     }
 
+    product_dic['total'] = price * quantity
+
     #agregar el diccionario a la lista
     products.append(product_dic)
-
-    print(f"\n✅ Producto: {name} agregado(a) con éxito !\n")
+    print(product_dic)
+    print(f"\n✅ Producto: {name} agregado(a) con éxito !")
 
     # Imprime cada uno de los registros:
 
@@ -34,7 +36,6 @@ def add_product():
     #     for clave, valor in producto.items():
     #         print(f"{clave}: {valor}")
 
-    
 
 
 # Function to search a product
@@ -44,13 +45,49 @@ def search_product(list_of_products):
 
     for product_searcher in list_of_products:
         if product_searcher['producto'] == search:
-            print(f"\nProducto: {product_searcher['producto']} | Precio: ${product_searcher ['precio']} | Cantidad: {product_searcher['cantidad']} unidades.")
+            print(f"\n🔍 Producto: {product_searcher['producto']} | Precio: ${product_searcher ['precio']} | Cantidad: {product_searcher['cantidad']} unidades.")
             founded = True
             break
     if not founded:
         print("El producto no se encuentra registrado en la base de datos.")
 
 
+# Function to update pr
+def update_price(list_of_products):
+    search = input("Nombre del producto a actualizar: \n").lower()
+    
+    for product_searcher in list_of_products:
+        if search == product_searcher['producto']:
+            #update_product = input("Actualiza el nombre del producto:    ").lower()
+            update_price = float(input("Actualiza su precio:    $"))
+            update_quatity = int(input("Actualiza su cantidad:  "))
+
+            #product_searcher['producto'] = update_product
+            product_searcher['precio'] = update_price
+            product_searcher['cantidad'] = update_quatity
+
+            print("\n✅ Actualización exitosa del producto.")
+            break
+
+
+# Function to delete products
+def delete_product(list_of_products):
+    
+    search = input("Busca por su nombre el producto que deseas eliminar:  ").lower()
+
+    for product_searcher in list_of_products:
+        if search == product_searcher['producto']:
+            del product_searcher['cantidad']
+            del product_searcher['precio']
+            del product_searcher['producto']
+            break
+    # mejorar el eliminar los diccionarios vacios.
+    print(list_of_products)
+
+
+def calculate_total_stock_price(list_of_products):
+    total = sum(product1['total'] for product1 in list_of_products)
+    print(f"El total de todo el inventario es: {total}")
 
 
 # Function to show the menu
@@ -67,21 +104,20 @@ def menu():
         option = int(input("Selecciona una opción del 1 al 6: \n"))
 
 
-
         if option == 1:
             # agregar inputs por clean code
             add_product()
         elif option == 2:
             search_product(products)
         elif option == 3:
-            update_price()
+            update_price(products)
         elif option == 4:
-            delete_product()
+            delete_product(products)
         elif option == 5:
-            calculate_total_stock_price()
+            calculate_total_stock_price(products)
         elif option == 6:
             print("Gracias por utilizar nuestro sistema. \n¡Hasta luego!")
-            print('*' * 60)
+            print('=' * 50)
             break
         else:
             print('Opción no válida. "Intenta de nuevo.')

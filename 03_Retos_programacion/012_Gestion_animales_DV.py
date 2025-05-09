5abd57d1fecbca1a10db4543d8fe46afdff11063
gepeto23 <gepeto23@gmail.com>
2025-05-09 13:00:24 -0500

    Se actualiza 012_gestion ambiental

--- 03_Retos_programacion/012_Gestion_animales_DV.py
+++ 03_Retos_programacion/012_Gestion_animales_DV.py
@@ -1,22 +1,65 @@
 
 # GESTION DE ANIMALES
-animales= []
-nombre = []
-edad = []
-enfermo = []
+baseDatos = []
+col1 = "NOMBRE"
+col2 = "EDAD"
+col3 = "ENFERMO"
+col4 = "CEDULA"
+ancho = 20
 
-def validarEdad (num):
+def __validarEdad (num):
     if num <= 0:
         print("Edad incorrecta")
 
-def agregar(name_x, age_x, sick_x):
-    nombre.append(name_x)
-    edad.append(age_x)
-    enfermo.append(sick_x)
+# Agregar animales a la BD
+def __agregar(name_x, age_x, sick_x, id_x):
+
+    if age_x >= 0 and sick_x == ("Si" or "No") and id_x >= 0:
+
+        animal = {
+            'nombre' : name_x,
+            'edad' : age_x,
+            'enfermo' : sick_x,
+            'cedula_id' : id_x
+        }
+
+        baseDatos.append(animal)
+
+        print("\nRegistro exitoso !")
+        print(baseDatos)
+    
+    else:
+        if age_x < 0 and sick_x != ("Si" or "No"):
+            print("El valor de edad es incorrecto y el estado de salud tambien es erroneo. Intenta de nuevo.")
+        elif age_x < 0:
+            print("Ha introducido una edad incorrecta. Intente de nuevo.")
+        elif sick_x != ("Si" or "No"):
+            print("El estado de salud debe ser unicamente SI o NO. Intente de nuevo.")
+        elif id_x < 0:
+            print("Ingrese un número válido de documento. Intente de nuevo.")
 
 
-def listarAnimales(lista_de_animales):
-    print(animales)
+
+def __buscarPorCedula():
+    
+
+
+# Funcion oara actualizar registro
+def __actualizarAnimal(listaNombres, listaEdades, listaEnfermos, listaCedulas_id):
+    
+    buscar_nombre = input("Nombre del animal a buscar: ")
+    
+
+
+
+
+# Funcion para listar toda la base de datos
+def __listarAnimales(lista_de_animales):
+    print(f'{col1.center(ancho)} | {col2.center(ancho)} | {col3.center(ancho)} | {col4.center(ancho)}')
+    for i in lista_de_animales:
+        print(f'{str(i['nombre']).center(ancho)} {str(i['edad']).center(ancho)} {str(i['enfermo']).center(ancho)} {str(i['cedula_id']).center(ancho)}')
+
+
 
 
 
@@ -26,7 +69,7 @@ def menu ():
     
     while True:
 
-        print("MENU")
+        print("\n----MENU----")
         print("| 1 | Agregar animal.")
         print("| 2 | Actualizar animal.")
         print("| 3 | Eliminar animal.")
@@ -38,20 +81,25 @@ def menu ():
         try:
             match opcion:
                 case "1":
+
                     print(f"\n--- AGREGAR ANIMALES ---")
                     name = input("Escribe el nombre del animal:  ").capitalize()
-                    age = int(input("Escribe la edad del animal:  ")) 
-                    (lambda x: print("Edad incorrecta") if x <= 0 else None)(age)
-                    sick = input("El animal está enfermo (Si/No):  ").capitalize()
 
-                    agregar(name, age, sick)
+                    age = int(input("Escribe la edad del animal:  ")) 
+
+                    sick = input("El animal está enfermo (Si/No):  ").capitalize()
+                    id_cedula = int(input("Ingresa el numero de documento del dueño:  "))
+
+                    __agregar(name, age, sick, id_cedula)
                     
                 case "2":
-                    pass
+                    __actualizarAnimal()
                 case "3":
                     pass
                 case "4":
-                    listarAnimales(animales)
+                    __buscarPorCedula(baseDatos)
+                case "5":
+                    __listarAnimales(baseDatos)
                 case "0":
                     pass
                 case _:
